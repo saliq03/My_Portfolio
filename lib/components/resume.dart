d3c82a284343136945faf6c71b8752ed91aef112
saliq03 <javidsaliq@gmail.com>
2024-06-09 17:51:55 +0530

    building resume

--- lib/components/resume.dart
+++ lib/components/resume.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:timelines/timelines.dart';
 
 class Resume extends StatelessWidget{
@@ -40,6 +41,8 @@ class Resume extends StatelessWidget{
     );
   }
 
+
+  //                     ---->    WORK   <-----
   Widget WorkExperience(){
     return Container(
       decoration: BoxDecoration(
@@ -80,58 +83,101 @@ class Resume extends StatelessWidget{
     );
   }
 
+
+//                    ----->   SKILLS    <------
   Widget Skills(){
+    return Padding(
+      padding: const EdgeInsets.only(left: 20,right: 20),
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(11),
+            color: Colors.white
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text('Tech Skills Skills',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 26,),),
+            Divider(),
+            SizedBox(height: 20,),
+            Wrap(
+                runSpacing: 10,
+                spacing: 10,
+                children: [
+                  SkillChip("Dart"),
+                  SkillChip("Java"),
+                  SkillChip("C++"),
+                  SkillChip("C"),
+                  SkillChip("Flutter"),
+                  SkillChip("Git"),
+                  SkillChip("Git Hub"),
+                  SkillChip("Figma"),
+                  SkillChip("My SQL"),
+                  SkillChip("SQLite"),
+                  SkillChip("Firebase"),
+                  SkillChip("Ubanto OS"),
+                  SkillChip("Vs Code"),
+                  SkillChip("Android Studio"),
+                   ],
+                  ),
+
+              ],
+            ),
+      ),
+    );
+  }
+
+  Widget SkillProgrammingLanguages(){
     return Container(
       decoration: BoxDecoration(
-        color: CupertinoColors.white,
-        borderRadius: BorderRadius.circular(4)
+      borderRadius: BorderRadius.circular(11),
+        color: Colors.white
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('Skills',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 26),),
-          SizedBox(height: 20,),
-          Wrap(
-            children: [
-              Column(
-                children: [
-                  Text("Programming languages",style: TextStyle(fontWeight: FontWeight.bold),),
-                  SizedBox(height: 10,),
-                  Chip(label: Text("Dart"),
-                       labelStyle: TextStyle(color: Colors.indigo),
-                       labelPadding: EdgeInsets.only(left: 3,right: 3,top: -3,bottom: -3),
-                       side: BorderSide(color: Colors.indigo),
-                       backgroundColor:Colors.white,),
+      width: 300,
+      child:  Padding(
+        padding: const EdgeInsets.only(top: 10,bottom: 10),
+        child: Column(
+          children: [
+            Text("Programming languages",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+            Divider(),
+            Wrap(
+              children: [
+                SkillChip("Dart"),
+                SkillChip("Java"),
+                SkillChip("C++"),
+                SkillChip("C"),
+              ],
+            )
 
-                  Text('java'),
-                  Text('c++'),
-                  Text('c'),
-                ],
-              ),
-              SizedBox(width: 20,),
-              Column(
-                children: [
-                  Text("Framworks",style: TextStyle(fontWeight: FontWeight.bold),),
-                  SizedBox(height: 10,),
-                  Text('Flutter'),
+          ],
+        ),
+      ),
+    );
+  }
 
-                ],
-              )
-            ],
-          ),
-        ],
+  Widget SkillChip(String data){
+    return Padding(
+      padding: const EdgeInsets.only(top: 3,bottom: 3,left:3,right: 3),
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 5,right: 5),
+          child: Text(data,style: TextStyle(color: Colors.indigo),),
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(6),
+            border:  Border.all(color: Colors.indigo,width: 1)
+        ),
       ),
     );
   }
 
 
 
+//                ------>       EDUCATION   <------
   Widget Education(){
     final List<Widget> events=[
       EducationBoxes('August 2021 - April 2025', 'Maulana Azad National Urdu University','Hyderabad,Telangana', 'B.tech- CS and IT', 'CGPA: 8.97 out of 10'),
       EducationBoxes('December 2020','Govt.Higher Secondary School','Nehalpora Pattan, J&K','12th-PCM , JKBOSE','Percentage: 91%'),
       EducationBoxes('December 2018','Hanfia Model High School','Warpora Pattan, J&K','10th, JKBOSE','Percentage: 92%'),];
-
     return Container(
       decoration: BoxDecoration(
         color: CupertinoColors.white,
